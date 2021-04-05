ea55df79e3867664c1226ff7db0534a7874d7722
bishalrai96 <bishal.rai64@Yahoo.com>
2021-04-05 21:39:07 +0100

    indecisive

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from wcag_zoo.validators.tarsier import Tarsier
+from selenium.webdriver.firefox.options import  Options
+import pickle
+import selenium.webdriver
+
+I_agree_button = "//button/div[contains(text(), 'I agree')]"
 
 URL = "http://127.0.0.1:8080/login.php"
 URL_index = "http://127.0.0.1:8080/index.php"
@@ -17,35 +22,30 @@ my_data = {'username': 'admin', 'password': 'password', "Login": "Login"}
 def check_header(html):
     instance = Tarsier()
     results = instance.validate_document(html.encode("utf-8"))
-    #print("Header validation")
-    #print(results)
-    print("\n")
+    f = open("C:\\programming\\results.txt2", "w+")
+    f.write(json.dumps(results, indent=4))
 
 
-def check_Anteater(html):
+def check_image_alternative_text(html):
     instance = Anteater()
     results = instance.validate_document(html.encode("utf-8"))
     f = open("C:\\programming\\results.txt", "w+")
     f.write(json.dumps(results, indent=4))
-    print("HTML image tags")
-    print(results)
-    print("\n")
 
 
 def get_parse(url, post_data=None, dvwa=False):
-    results = list()
     with requests.Session() as s:
         page = s.get(url, proxies=proxies, verify=False)
         soup = BeautifulSoup(page.content, "html.parser")
 
-        get_css(soup, url)
+        new_dir = download_css(soup, url)
         if dvwa:
             user_token = soup.find_all(attrs={"name": "user_token"})
             # get user token sent from client side
             my_data["user_token"] = user_token[0].get("value")
 
         check_header(page.text)
-        check_Anteater(page.text)
+        check_image_alternative_text(page.text)
 
         if post_data is not None:
             send = s.post(URL, data=my_data, proxies=proxies, verify=False)
@@ -65,7 +65,8 @@ def get_absolute_css_url(url, css_path):
     return url
 
 
-def get_css(soup, url):
+def download_css(soup, url):
+    dir_name = None
     css_tags = soup.findAll("link", href=True)
     print("HERE")
     for css_tag in css_tags:
@@ -74,26 +75,33 @@ def get_css(soup, url):
             css_url = get_absolute_css_url(url, css_path)
             css_content = requests.get(css_url).text
 
-            dir_name = os.getcwd()
-            abs_filepath = os.path.join(dir_name, css_path)
+            working_dir_name = os.getcwd()
+            abs_filepath = os.path.join(working_dir_name, css_path)
+            print(os.path.dirname(abs_filepath))
+
+            dir_name = os.path.dirname(abs_filepath)
 
             if not os.path.exists(os.path.dirname(abs_filepath)):
                 os.makedirs(os.path.dirname(abs_filepath))
+                dir_name = os.path.dirname(abs_filepath)
 
             f = open(abs_filepath, "w+")
             f.write(str(css_content))
 
-    # for css_tag in css_tags:
+    return dir_name
 
-    pass
+def get_cookie():
+    options = Options()
+    options.add_argument('--headless')
+    driver = selenium.webdriver.Firefox()
+    driver.get("http://www.google.com")
+    print(driver.get_cookies())
+    #cpickle.dump(driver.get_cookies(), open("cookies.pkl"), "wb")
 
 
-def clear_up():
-    pass
-
-
-def get_js(html):
-    pass
-
+get_cookie()
+def remove_dir(directory):
+    import shutil
+    shutil.rmtree(directory)
 
 get_parse("https://www.w3.org/WAI/demos/bad/before/home.html")
